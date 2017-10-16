6d276999b28569afee6dfa794541347869b9d619
turizsolt <turi.zsolt.m@gmail.com>
2017-10-17 00:19:19 +0200

    Try to test the controller. Part 2.

--- http-api/test/todo-list.test.ts
+++ http-api/test/todo-list.test.ts
@@ -1,23 +1,31 @@
+import 'reflect-metadata';
+
 import * as chai from 'chai';
 const expect = chai.expect;
 import TodoListController from "../src/todo-list-controller";
 import * as sinon from 'sinon';
 import * as sinonChai from 'sinon-chai';
+import TodoList from "../../core/src/TodoList";
+import {myContainer} from "../../inversify.config";
+import {TYPES} from "../../types";
 
 chai.use(sinonChai);
 
 describe("TodoListController", () => {
 
     it("Get all todoes", () => {
-        const todoListController = new TodoListController();
+        const todoList = {getAll: sinon.spy()};
         const responseSend = sinon.spy();
+
+        const todoListController = new TodoListController(todoList as any);
         todoListController.getAll(null, {send: responseSend});
 
-        expect(responseSend).calledWith([]);
+        expect(todoList.getAll).called;
+        expect(responseSend).called;
     });
 
     it("Get a todo", () => {
-        const todoListController = new TodoListController();
+        const todoListController = new TodoListController(myContainer.get<TodoList>(TYPES.TodoList));
         const responseSend = sinon.spy();
         todoListController.get({params:{id:1}}, {send: responseSend});
 
--- inversify.config.ts
+++ inversify.config.ts
@@ -2,6 +2,7 @@ import { Container } from "inversify";
 import {TYPES} from "./types";
 import TodoList from "./core/src/TodoList";
 import InMemoryTodoList from "./core/src/InMemoryTodoList";
+import TodoListController from "./http-api/src/todo-list-controller";
 
 const myContainer = new Container();
 myContainer.bind<TodoList>(TYPES.TodoList).to(InMemoryTodoList);
