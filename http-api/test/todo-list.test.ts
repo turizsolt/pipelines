a6e0c351ac455726619c78c05d375c71f0e80705
turizsolt <turi.zsolt.m@gmail.com>
2017-10-16 23:53:13 +0200

    Try to test the controller.

--- http-api/test/todo-list.test.ts
+++ http-api/test/todo-list.test.ts
@@ -1,8 +1,26 @@
-import {expect} from 'chai';
+import * as chai from 'chai';
+const expect = chai.expect;
+import TodoListController from "../src/todo-list-controller";
+import * as sinon from 'sinon';
+import * as sinonChai from 'sinon-chai';
+
+chai.use(sinonChai);
 
 describe("TodoListController", () => {
 
     it("Get all todoes", () => {
-        const todoListController =
+        const todoListController = new TodoListController();
+        const responseSend = sinon.spy();
+        todoListController.getAll(null, {send: responseSend});
+
+        expect(responseSend).calledWith([]);
+    });
+
+    it("Get a todo", () => {
+        const todoListController = new TodoListController();
+        const responseSend = sinon.spy();
+        todoListController.get({params:{id:1}}, {send: responseSend});
+
+        expect(responseSend).calledWith({ text: undefined });
     });
 });
